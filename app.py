732a9127b79318b161ba1d7a76031c29446ecdbc
Rémy Schumm @kassandra/noser <remy.schumm@noser.com>
2019-01-29 17:04:59 +0100

    pw

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import time
+import os
 
 import redis
 from flask import Flask
 
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(host='redis', port=6379, password=os.getenv('REDIS_PW')
 
 
 def get_hit_count():
