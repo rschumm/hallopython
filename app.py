0217d25c5433294c4cb0b31fc20384640cd65f38
Rémy Schumm @kassandra/noser <remy.schumm@noser.com>
2019-01-29 17:09:10 +0100

    syntax

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379, password=os.getenv('REDIS_PW')
+cache = redis.Redis(host='redis', port=6379, password=os.getenv('REDIS_PW'))
 
 
 def get_hit_count():
